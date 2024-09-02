e9eb9aa5216b44b0753ed331433d74349f184ea7
JoaBarreraCalix <joansebastianbusiness@gmail.com>
2024-09-02 15:31:01 -0500

    Creacion de proyecto spring

---
+++ src/main/java/com/example/emazonusers/EmazonusersApplication.java
@@ -0,0 +1,13 @@
+package com.example.emazonusers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmazonusersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmazonusersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/emazonusers/EmazonusersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.emazonusers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmazonusersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
